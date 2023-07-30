0fb7599f195037978a276b503b027625113c5126
empty127 <140940090+empty127@users.noreply.github.com>
2023-07-31 00:12:06 +0300

    main.py

--- example_bot/main.py
+++ example_bot/main.py
@@ -2,13 +2,11 @@ from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 
 PROXY_URL = "http://proxy.server:3128"
 bot = Bot(token=os.environ.get('TOKEN'), proxy=PROXY_URL)
 dp = Dispatcher(bot)
-
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -17,14 +15,9 @@ async def send_welcome(message: types.Message):
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
-
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
-    await message.answer(message.text)
-
+    await bot.send_message("@statsjob", message.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
